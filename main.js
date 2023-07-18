121099f051ebe4be3a547b0011e796d2a2f04d11
fadywadea <fwadea3@gmail.com>
2023-07-18 19:02:56 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,87 @@
+// window.alert("bteeeeeeeeeeeeeeeeeee5");
+// document.getElementById("demo").innerHTML = "bteeeeeeeeeeeeeeeeeee5";
+// console.log("bteeeeeeeeeeeeeeeeeee5");
+/*--------------------------------------------------------------------------------*/
+
+// var userRole = window.prompt("enter ur role");
+// var userAge = 20;
+
+// if (userRole === "admin") {
+//   console.log("u can do anything");
+//   if (userAge > 18) {
+//     console.log("welcom");
+//   }
+// } else if (userRole === "editor") {
+//   console.log("u can edit");
+// } else if (userRole === "viewer") {
+//   console.log("u can view");
+// } else {
+//   console.log("invalid input");
+// }
+
+/*--------------------------------------------------------------------------------*/
+
+
+
+// var numb11 = prompt("Enter a number :");
+
+// numb11 = Number(numb11);
+
+// for (var i = 1; i <= numb11; i++) {
+//   if ((numb11 * i) % 2 == 0) {
+//   console.log("even numbers");
+//   } else {
+//     console.log("odd number");
+//   }
+/*--------------------------------------------------------------------------------*/
+
+
+
+// if (typeof price == "string") {
+//   price = Number(price)
+// }
+// }
+
+
+/*--------------------------------------------------------------------------------*/
+
+// function calcproductPrice(price,profit,vat) {
+//   price = prompt("Enter a numebr price :");
+//   profit = prompt("Enter a numebr profit :");
+//   vat = prompt("Enter a numebr vat :");
+//   var priceWithProfit = price + profit;
+//   var finalPrice = priceWithProfit + priceWithProfit * vat;
+//   console.log(finalPrice, "finalPrice");
+// }
+
+// calcproductPrice(40000,2000,0.2)
+
+
+// var numb1 = prompt("Enter the first integer: ");
+// var numb2 = prompt("Enter the second integer: ");
+// var result = (numb1 ^ numb2);
+
+// numb1 = Number(numb1);
+// numb2 = Number(numb2);
+// result = Number(result);
+
+// console.log(result);
+
+/*--------------------------------------------------------------------------------*/
+
+// var number = prompt("Enter a number: ");
+// switch (number % 2) {
+//   case 0:
+//     console.log("The number is even.");
+//     break;
+//   case 1:
+//     console.log("The number is odd.");
+//     break;
+// }
+/*--------------------------------------------------------------------------------*/
+function calcSum(x, y) {
+  var res = x + y;
+  console.log(res);
+}
+
+calcSum(100, 100)
